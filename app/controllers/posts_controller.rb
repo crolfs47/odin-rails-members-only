7978f42340b4ca86ec7b73b077046c21f9dd695a
Cathy Rolfs <crolfs47@gmail.com>
2023-01-24 18:30:13 -0600

    Add #new, #create, #index actions in Posts Controller. Add index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,23 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new # status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
